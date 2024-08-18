aabe0d09c87fab28ee5dc384786f7e10f29e9703
ciasmim <Athena@24>
2024-08-18 01:50:48 -0300

    Add widget consulta saldo investments

--- lib/presentation/pages/investments/investments.dart
+++ lib/presentation/pages/investments/investments.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:get/get_connect/http/src/utils/utils.dart';
 import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
+import 'package:nubank_interface/utils/colors_standart.dart';
 
 class Investments extends StatelessWidget {
   const Investments({super.key});
@@ -13,6 +15,7 @@ class Investments extends StatelessWidget {
         children: [
           _textInvestmentsChevron(),
           _investmentesValue(),
+          _consultValue(),
         ],
       ),
     );
@@ -31,9 +34,31 @@ class Investments extends StatelessWidget {
 
   _investmentesValue() {
     return const Padding(
-        padding:  EdgeInsets.only(top: 16.0),
-        child:  Text(
+        padding: EdgeInsets.only(top: 16.0),
+        child: Text(
             'O jeito Nu de investir: que cabe no seu bolso, \n a partir de R\$1. Saiba mais.',
             style: TextStyle(color: Colors.grey)));
+  }
+
+  _consultValue() {
+    return Container(
+       margin: const EdgeInsets.only(top: 12, bottom: 12),
+       decoration: BoxDecoration(
+        color: greyColor, borderRadius: BorderRadius.circular(15)
+       ),
+       padding: const EdgeInsets.all(18),
+      child: Row(
+        children: [
+          Icon(MdiIcons.cash),
+          Container(
+            margin: const EdgeInsets.symmetric(horizontal: 10),
+            child: const Text(
+              'Consultar saldo para transferência',
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
